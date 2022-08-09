8d42b8098590f4a3a2a5dc861a147bd614a1063e
Ademuyiwa Adetunji <nobelatunje001@gmail.com>
2022-08-09 23:23:11 +0100

    setup phpinsights

---
+++ config/insights.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+
+    'preset' => 'laravel',
+
+    'ide' => 'phpstorm',
+
+    'exclude' => [
+        'app/Providers'
+    ],
+
+    'add' => [],
+
+    'remove' => [
+        // Code
+        \SlevomatCodingStandard\Sniffs\TypeHints\DeclareStrictTypesSniff::class,          // Declare strict types
+        \SlevomatCodingStandard\Sniffs\Classes\ForbiddenPublicPropertySniff::class,       // Forbidden public property
+        \PhpCsFixer\Fixer\ClassNotation\VisibilityRequiredFixer::class,                   // Visibility required
+        \SlevomatCodingStandard\Sniffs\Classes\ClassConstantVisibilitySniff::class,       // Class constant visibility
+        \SlevomatCodingStandard\Sniffs\ControlStructures\DisallowEmptySniff::class,       // Disallow empty
+        \PhpCsFixer\Fixer\Comment\NoEmptyCommentFixer::class,                             // No empty comment
+        \SlevomatCodingStandard\Sniffs\Commenting\UselessFunctionDocCommentSniff::class,  // Useless function doc comment
+
+        // Architecture
+        \NunoMaduro\PhpInsights\Domain\Insights\ForbiddenNormalClasses::class,            //Normal classes are forbidden
+
+        // Style
+        \PHP_CodeSniffer\Standards\Generic\Sniffs\Formatting\SpaceAfterCastSniff::class,  // Space after cast
+        \PHP_CodeSniffer\Standards\Generic\Sniffs\Formatting\SpaceAfterNotSniff::class,   // Space after not
+        \SlevomatCodingStandard\Sniffs\Namespaces\AlphabeticallySortedUsesSniff::class,   // Alphabetically sorted uses
+        \SlevomatCodingStandard\Sniffs\Commenting\DocCommentSpacingSniff::class,          // Doc comment spacing
+        \PhpCsFixer\Fixer\ClassNotation\OrderedClassElementsFixer::class,                 // Ordered class elements
+        \PhpCsFixer\Fixer\StringNotation\SingleQuoteFixer::class,                         // Single quote
+    ],
+
+    'config' => [
+        \NunoMaduro\PhpInsights\Domain\Insights\CyclomaticComplexityIsHigh::class => [
+            'maxComplexity' => 8,
+        ],
+        \PHP_CodeSniffer\Standards\Generic\Sniffs\Files\LineLengthSniff::class => [
+            'lineLimit' => 120,
+            'absoluteLineLimit' => 120,
+            'ignoreComments' => false,
+        ],
+        \PhpCsFixer\Fixer\Import\OrderedImportsFixer::class => [
+            'imports_order' => ['class', 'const', 'function'],
+            'sort_algorithm' => 'length',
+        ]
+    ],
+
+    'requirements' => [
+        'min-quality' => 75,
+        'min-complexity' => 75,
+        'min-architecture' => 75,
+        'min-style' => 75,
+        'disable-security-check' => false,
+    ],
+];

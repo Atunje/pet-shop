b2845e2d23c07ad65c2a381a585c68f6d2114a81
Ademuyiwa Adetunji <nobelatunje001@gmail.com>
2022-08-10 23:38:14 +0100

    created migration files for users and jwt_tokens

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,23 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->bigIncrements('id', true);
+            $table->uuid('uuid');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->boolean('is_admin');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('avatar');
+            $table->text('address');
+            $table->string('phone_number')->unique();
+            $table->boolean('is_marketing');
             $table->timestamps();
+            $table->timestamp('last_login_at')->nullable();
+            $table->timestamp('deleted_at')->nullable();
+
+            $table->index(['uuid', 'is_admin']);
         });
     }
 
---
+++ database/migrations/2022_08_10_223449_create_jwt_tokens_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jwt_tokens', function (Blueprint $table) {
+            $table->bigIncrements('id', true);
+            $table->string('unique_id')->unique();
+            $table->bigInteger('user_id')->unsigned();
+            $table->string('token_title');
+            $table->boolean('is_valid')->default(false);
+            $table->string('restrictions')->nullable();
+            $table->string('permissions')->nullable();
+            $table->timestamps();
+            $table->timestamp('expires_at')->nullable();
+            $table->timestamp('last_used_at')->nullable();
+            $table->timestamp('refreshed_at')->nullable();
+        });
+
+        Schema::table('jwt_tokens', function($table) {
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jwt_tokens');
+    }
+};

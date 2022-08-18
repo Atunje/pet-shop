e20d728e5d090d882f2037793ca4f79c0ab66918
Ademuyiwa Adetunji <nobelatunje001@gmail.com>
2022-08-18 21:03:43 +0100

    Fixed middlewares to protect admin and user routes

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Validation\UnauthorizedException;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure $next
+     * @return Response|RedirectResponse|JsonResponse
+     */
+    public function handle(Request $request, Closure $next): Response|RedirectResponse|JsonResponse
+    {
+        $user = $request->user();
+
+        if ($user !== null && $user->isAdmin()) {
+            return $next($request);
+        }
+
+        throw new UnauthorizedException();
+    }
+}

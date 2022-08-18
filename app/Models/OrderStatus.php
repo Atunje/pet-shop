e980e23727b585c4af89eab78071d995c4f15326
Ademuyiwa Adetunji <nobelatunje001@gmail.com>
2022-08-18 21:56:26 +0100

    ide helper model annotations

--- app/Models/OrderStatus.php
+++ app/Models/OrderStatus.php
@@ -8,6 +8,30 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+/**
+ * App\Models\OrderStatus
+ *
+ * @property int $id
+ * @property string $uuid
+ * @property string $title
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @method static \Database\Factories\OrderStatusFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderStatus newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderStatus newQuery()
+ * @method static \Illuminate\Database\Query\Builder|OrderStatus onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderStatus query()
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderStatus whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderStatus whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderStatus whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderStatus whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderStatus whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderStatus whereUuid($value)
+ * @method static \Illuminate\Database\Query\Builder|OrderStatus withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|OrderStatus withoutTrashed()
+ * @mixin \Eloquent
+ */
 class OrderStatus extends Model
 {
     use HasFactory, HasUUIDField, FilterableModel, SoftDeletes;

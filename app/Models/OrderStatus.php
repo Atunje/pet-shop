eef900ccf49226b688825b89bd4d1e1adad93473
Ademuyiwa Adetunji <nobelatunje001@gmail.com>
2022-08-18 09:10:09 +0100

    Fixed Order Status isShippedStatus method

--- app/Models/OrderStatus.php
+++ app/Models/OrderStatus.php
@@ -16,4 +16,34 @@ class OrderStatus extends Model
         'title',
         'slug',
     ];
+
+    /**
+     * Status of shipped order
+     *
+     * @const string
+     */
+    private const ORDER_SHIPPED_STATUS = "shipped";
+
+    /**
+     * Get order status by uuid
+     *
+     * @param string $order_status_uuid
+     * @return ?self
+     */
+    public static function getStatus($order_status_uuid)
+    {
+        return self::where('uuid', $order_status_uuid)->first();
+    }
+
+    /**
+     * Check if status is shipped
+     *
+     * @param string $uuid
+     * @return bool
+     */
+    public static function isShippedStatus($uuid): bool
+    {
+        $status = self::getStatus($uuid);
+        return $status !== null && strtolower($status->title) === self::ORDER_SHIPPED_STATUS;
+    }
 }
